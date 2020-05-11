6c91fec664d18a40b8571931093fa04c1728cbbb
gp34446 <48416325+gp34446@users.noreply.github.com>
2020-05-11 14:55:06 -0300

    re para expensas

--- expensas.py
+++ expensas.py
@@ -4,28 +4,29 @@ import re
 
 data = pd.read_csv('../desafio_ds/properati.csv')
 
-def expensas(df,input=0):
-    if str(input) == '0':
-        pattern = 'SIN EXPENSA|NO HAY EXPENSA|NO TIENE EXPENSA'
-    else:
-        pattern = '(?P<expensa>expensa*).{1,20}(?P<moneda>\$\D*)(?P<monto>\d+)'
+def expensas(df):
+    cont = data['expenses'].isna().sum()
+    cont_orig = cont
+    pattern = '(SIN\sEXPENSA|NO\sHAY\sEXPENSA|NO\sTIENE\sEXPENSA)'
+    patron_sin_ex = re.compile(pattern, re.IGNORECASE)
+    search = df['description'].apply(lambda x: x if x is np.NaN else patron_sin_ex.search(re.sub('\.', '', x)))
+    mascara_search = (search.notnull()) & df['expenses'].isnull()
+    df.loc[mascara_search, 'expenses'] = 0
+    resultado = cont - data['expenses'].isna().sum()
+    cont = data['expenses'].isna().sum()
+    print('Se completaron con el patron SIN EXPENSA: {} registros'.format(resultado))
+
+    pattern = '(?P<expensa>expensa.{1,20})(?P<moneda>\$\D*)(?P<monto>\d+)'
     patron_sin_ex = re.compile(pattern,re.IGNORECASE)
     search = df['description'].apply(lambda x:x if x is np.NaN else patron_sin_ex.search(re.sub('\.','',x)))
     mascara_search = (search.notnull()) & df['expenses'].isnull()
-    if str(input) == '0':
-        df.loc[mascara_search,'expenses'] = 0
-    else:
-        df.loc[mascara_search,'expenses'] = search[mascara_search].apply(lambda x: float(x.group('monto')))
-
-data['expenses2'] = data['expenses']
-print(data['expenses'].isna().sum())
-expensas(data,input = 'nro')
-print(data['expenses'].isna().sum())
-expensas(data,input = '0')
-print(data['expenses'].isna().sum())
-print(data.loc[74,'expenses'])
-mascara = (data['expenses'] > 50000) & (data['expenses'].notnull()) & (data['expenses2'].isnull())
-print(data.loc[mascara,['expenses','expenses2']])
-
+    df.loc[mascara_search,'expenses'] = search[mascara_search].apply(lambda x: float(x.group('monto')))
+    resultado = cont - data['expenses'].isna().sum()
+    cont = data['expenses'].isna().sum()
+    print('Se completaron con el patron EXPENSAS MONTO: {} registros'.format(resultado))
+    print('Total original de NULLs para expenses: {}'.format(cont_orig))
+    print('Total actual de NULLs para expenses: {}'.format(cont))
+    print('Porcentaje de NULLs corregidos para expenses: {}%'.format(round((100 - (cont * 100) / cont_orig)), 0))
+expensas(data)
 
 
