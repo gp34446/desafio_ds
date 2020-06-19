cd6dc3a73e3c6831b981efb2285ce2f45983d63f
gp34446 <48416325+gp34446@users.noreply.github.com>
2020-06-19 11:11:16 -0300

    presentacion

--- reg_lineal.py
+++ reg_lineal.py
@@ -7,7 +7,7 @@ from sklearn.feature_selection import RFE
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-def corr_filter(df,y, val=0.0001):
+def corr_filter(df,y= 'price_usd_per_m2', val=0.001):
  x = df.drop(y, axis = 1)
  lista = []
  for i in x.columns:
@@ -17,28 +17,35 @@ def corr_filter(df,y, val=0.0001):
  return lista
 """SELECCIONO CAMPOS Y APLICAMOS DUMMIES"""
 df = pd.read_csv('../desafio_ds/df_clean.csv')
-df = df[df['lat'].notnull()]
-col = ['property_type', 'place_name',  'state_name','lat','lon',\
+
+
+def ridge(data,state_name, target='price_usd_per_m2', val_corr=0.001):
+ df = pd.get_dummies(data, drop_first=True)
+ lista = corr_filter(df, target, val_corr)
+ lm_ridge = linear_model.RidgeCV(alphas=[0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.005, 0.01,\
+                                        0.05, 0.1, 1, 5, 10], normalize=True, cv=5)
+ y = df[target]
+ X = df[lista]
+ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=100)
+ lm_ridge.fit(X_train, y_train)
+ y_predicted = lm_ridge.predict(X_test)
+ dicc = {'state_name':st,'r2':metrics.r2_score(y_test, y_predicted),'observaciones':df.shape[0]}
+ print(dicc)
+ return dicc
+
+col = ['property_type', 'place_name',  'state_name',\
  'surface_total_in_m2', 'price_usd_per_m2', 'floor', 'rooms', 'expenses', \
  'pileta|piscina', 'terraza|solarium', 'cochera|garage', 'patio|jardin', 'laundry|lavadero', \
  'parrilla|churrasquera|asadera', 'amenities']
-#'lat','lon',
+
 df = df[col]
-df = pd.get_dummies(df, drop_first=True)
-lista = corr_filter(df,'price_usd_per_m2')
-print(lista)
-"""MODELO REG LINEAL"""
+lista = []
+iterador = df['state_name'].unique().tolist()
+for st in iterador:
+  df_aux = df[df['state_name'] == st]
+  if df_aux.shape[0] > 100:
+   df_aux = df_aux.drop('state_name', axis=1)
+   lista.append(ridge(df_aux,state_name = st))
 
-lm_ridge = linear_model.RidgeCV(alphas=[0.1, 1, 10], normalize=True)
-#rfe = RFE(lm_ridge, 20)
-y = df['price_usd_per_m2']
-X = df[lista]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=179798700)
-
-lm_ridge.fit(X_train, y_train)
-
-y_predicted = lm_ridge.predict(X_test)
-
-print(metrics.r2_score(y_test, y_predicted))
-
+df_final = pd.DataFrame(lista)
+df_final.to_csv('../desafio_ds/df_score.csv')
